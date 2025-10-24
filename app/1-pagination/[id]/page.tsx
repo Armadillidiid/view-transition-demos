1849042e19e937ef57ef4f68afa818b8ee85a2a7
Emmanuel Isenah <emmanuelisenah@gmail.com>
2025-10-24 14:13:12 +0100

    chore: disable cacheComponent in next config

--- app/1-pagination/[id]/page.tsx
+++ app/1-pagination/[id]/page.tsx
@@ -2,14 +2,12 @@ import { products } from "@/lib/products";
 import { default as PageClient } from "./page-client";
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
-  "use cache";
-
   const { id } = await props.params;
   return <PageClient productId={id} products={products} />;
 }
 
 export async function generateStaticParams() {
   return products.map((product) => ({
-    params: { id: product.id },
+    params: { id: product.id.toString() },
   }));
 }
--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   typedRoutes: true,
-  cacheComponents: true,
-  experimental: {
-    viewTransition: true
-  }
 };
 
 export default nextConfig;
